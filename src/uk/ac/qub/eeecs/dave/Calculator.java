b655d296fc2a9715d62889852aeb4d027fa4b8e4
Zara Moore <zarammoore@gmail.com>
2022-07-26 14:53:44 +0100

    error fixed

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -3,7 +3,7 @@ package uk.ac.qub.eeecs.dave;
 // calculator class to add and subtract numbers 
 public class Calculator {
 	public int add(int a, int b) {
-		return a+b+1;
+		return a+b;
 	}
 	
 	public int sub(int a, int b) {
